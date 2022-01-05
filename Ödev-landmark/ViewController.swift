1758214b6188820ad0bc62ac077125dad85c5727
tugrul95 <tugrul1995@gmail.com>
2022-01-05 16:00:18 +0300

    Initial Commit

---
+++ Ödev-landmark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ödev-landmark
+//
+//  Created by Macbook on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
